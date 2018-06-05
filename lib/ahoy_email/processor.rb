24edc173e35901bda29cb17f0f57f8441ba7aed3
Gustav E Gennrich <gustav@sreducationgroup.org>
2018-06-05 14:47:10 -0700

    revert

--- lib/ahoy_email/processor.rb
+++ lib/ahoy_email/processor.rb
@@ -96,7 +96,7 @@ module AhoyEmail
     def track_links
       if html_part?
         body = (message.html_part || message).body
-        doc = Nokogiri::HTML(body.to_s)
+        doc = Nokogiri::HTML(body.raw_source)
         doc.css("a[href]").each do |link|
           uri = parse_uri(link["href"])
           next unless trackable?(uri)
